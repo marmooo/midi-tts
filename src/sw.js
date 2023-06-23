f28e24baa5592a7bf140f2822cdd86ad590bab42
marmooo <marmooo@users.noreply.github.com>
2023-06-23 09:24:45 +0900

    update

--- src/sw.js
+++ src/sw.js
@@ -1,11 +1,10 @@
-var CACHE_NAME = "2023-06-07 09:45";
+var CACHE_NAME = "2023-06-23 09:25";
 var urlsToCache = [
   "/midi-tts/",
   "/midi-tts/index.js",
   "/midi-tts/mora.lst",
   "/midi-tts/Ritsu_0.0.2.sf3",
   "/midi-tts/favicon/favicon.svg",
-  "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css",
   "https://cdn.jsdelivr.net/npm/midi-writer-js@2.1.4/browser/midiwriter.min.js",
   "https://cdn.jsdelivr.net/npm/js-synthesizer@1.8.5/dist/js-synthesizer.min.js",
   "https://cdn.jsdelivr.net/npm/js-synthesizer@1.8.5/dist/js-synthesizer.worklet.min.js",
